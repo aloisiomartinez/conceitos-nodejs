9c2ace6cdb35873179c00000c5cf846b2ed6b5d1
aloisiomartinez <aloisiojacobs36@gmail.com>
2020-04-12 21:00:01 -0300

    Projects CRUD, likes

--- src/app.js
+++ src/app.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-
-const { uuid } = require("uuidv4");
+const { uuid } = require('uuidv4')
 
 const app = express();
 
@@ -11,7 +10,7 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+  return response.json(repositories);
 });
 
 app.post("/repositories", (request, response) => {
@@ -31,11 +30,40 @@ app.post("/repositories", (request, response) => {
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+  const { title, url, techs } = request.body;
+  
+  const repositoryIndex = repositories.findIndex(repository => repository.id === id);
+
+  if (repositoryIndex < 0) {
+    return response.status(400).json({ error: 'Invalid Project ID.' });
+  }
+  
+  const repository = {
+    id,
+    title,
+    url,
+    techs
+  };
+  
+  repositories[repositoryIndex] = repository;
+
+  return response.json(repository);
+
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+  
+  const repositoryIndex = repositories.findIndex(repository => repository.id === id);
+
+  if (repositoryIndex < 0) {
+    return response.status(400).json({ error: 'Invalid Project ID.' });
+  }
+
+  repositories.splice(repositoryIndex, 1);
+
+  return response.status(204).send();
 });
 
 app.post("/repositories/:id/like", (request, response) => {
@@ -43,9 +71,13 @@ app.post("/repositories/:id/like", (request, response) => {
 
   const repository = repositories.find(repository => repository.id === id);
 
+  if (!repository) {
+    return response.status(400).json({ error: 'Repository does not exist.'})
+  };
+
   repository.likes += 1;
 
-  return response.json(repository)
+  return response.json(repository);
 });
 
 module.exports = app;
